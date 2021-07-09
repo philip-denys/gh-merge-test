17104e04f261880282ee771362aea8ae4e37c489
philipdenys <philip.denys@logitail.com>
2021-07-09 09:14:40 +0200

    omg... my first code with copilot AI

---
+++ index.js
@@ -0,0 +1,7 @@
+function calculateDaysBetweenDates(begin, end) {
+    var beginDate = new Date(begin);
+    var endDate = new Date(end);
+    var days = Math.round((endDate - beginDate) / (1000 * 60 * 60 * 24));
+    return days;
+}
+console.log(calculateDaysBetweenDates('2015-01-01', '2015-01-31'))
